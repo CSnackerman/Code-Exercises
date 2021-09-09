38d4f670fc3fc1f153e6184e27851c611e689f71
Colby <colbyackerman@gmail.com>
2021-09-09 15:28:32 -0500

    setup 5 novice exercises

--- Javascript Exercises/Novice/script.js
+++ Javascript Exercises/Novice/script.js
@@ -1,4 +1,74 @@
 /* EXERCISE 1 */
 
 // Instructions:
-// Programmatically create an <h2> html element
+// 1. Create a variable named 'myFavoriteNumber' with 'var' keyword
+// 2. Assign a number value to it
+// 3. Create a variable with 'let' keyword & give it the identifier 'mostInterestingWord'
+// 4. Set it equal to a string value.
+
+
+
+
+
+
+
+/* EXERCISE 2 */
+
+// Instructions:
+// 1. Use console.log to output the values of your variables
+// 2. Add another argument within console.log which labels the variable in the console
+
+
+
+
+
+
+
+/* EXERCISE 3 */
+
+// Instructions:
+// 1. Programmatically create an <h2> html element
+// 2. Set the innerHTML contents of the element to anything you like
+// 3. append it to the body of the html page
+
+
+
+
+
+
+/* EXERCISE 4 */
+
+// Let's talk about TREES
+//      * They are everywhere in computers.
+//      * Surely the largest forest on earth exists within the memory of computers.
+//
+// Where do they crop up?
+// 1. 
+// 2.
+
+// Instructions:
+// 1. get the root element of the HTML file (body)
+// 2. console log it
+// 3. Use the inspector to view the element
+
+
+
+
+
+
+
+/* EXERCISE 5 */
+
+// Now let's add some style to your <h2> element programmatically
+// Every stylizer in CSS is available in Javascript!
+
+// 1. modify the style of one of your elements by using the .style operator
+//      e.g. 
+//          your_element.style.color = '#FFFFFF'
+//          your_element.style.fontSize = '100pt'
+//          your_element.style.borderStyle = 'solid orange 10px'
+
+// check here for more modifiable styles: 
+// https://www.w3schools.com/cssref/default.asp
+
+
